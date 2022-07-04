542b968c97b3e288726bfd12ca1a45764e956fa3
Angel Martinez <angel.martineztrc@gmail.com>
2022-07-04 18:19:38 -0500

    feat:  create a base entity

--- api/src/core/entities/base.entity.ts
+++ api/src/core/entities/base.entity.ts
@@ -1,7 +1,23 @@
+import {
+  Column,
+  CreateDateColumn,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+
 export abstract class BaseEntity {
+  @PrimaryGeneratedColumn()
   id: number;
+
+  @Column()
   enable: boolean;
+
+  @CreateDateColumn()
   createdAt: number;
+
+  @UpdateDateColumn()
   updatedAt: number;
+
+  @Column({ default: null, nullable: true })
   deletedAt: number;
 }
