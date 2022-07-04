439303fd68062d9e921819a6485c6a68be209547
Angel Martinez <angel.martineztrc@gmail.com>
2022-07-04 17:47:48 -0500

    chore:  generate resources

--- api/src/app/app.module.ts
+++ api/src/app/app.module.ts
@@ -2,11 +2,15 @@ import { Module } from '@nestjs/common';
 
 import { ConfigModule } from '@/core/config/config.module';
 
+import { GuidesModule } from './guides/guides.module';
+import { UsersModule } from './users/users.module';
+import { WorkspacesModule } from './workspaces/workspaces.module';
+
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [ConfigModule],
+  imports: [ConfigModule, GuidesModule, UsersModule, WorkspacesModule],
   controllers: [AppController],
   providers: [AppService],
 })
